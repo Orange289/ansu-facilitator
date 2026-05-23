c8faca4f4978613a8c057c832addd210eeb746d8
Anna <ansu@exante.eu>
2026-05-23 17:13:59 +0300

    test

--- src/components/site/footer.tsx
+++ src/components/site/footer.tsx
@@ -66,7 +66,7 @@ export function Footer() {
               )
             })}
           </div>
-          <p className="text-xs text-ink/52">{common("copyright")}</p>
+          <p className="text-xs text-ink/52">{common("copyright")}</p>test
         </div>
       </div>
     </footer>
